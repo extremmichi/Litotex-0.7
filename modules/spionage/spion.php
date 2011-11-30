0cef2a57ba5721684c398bd8611e7eadfeefaa48
extremmichi <extremmichi@yahoo.de>
2011-11-30 19:40:31 +0100

    Update modules/spionage/spion.php

--- modules/spionage/spion.php
+++ modules/spionage/spion.php
@@ -8,13 +8,13 @@ http://www.freebg.de
 Copyright (c) 2008 FreeBG Team
 ************************************************************
 Hinweis:
-Diese Software ist urheberrechtlich geschützt.
+Diese Software ist urheberrechtlich geschï¿½tzt.
 
-Für jegliche Fehler oder Schäden, die durch diese Software
-auftreten könnten, übernimmt der Autor keine Haftung.
+Fï¿½r jegliche Fehler oder Schï¿½den, die durch diese Software
+auftreten kï¿½nnten, ï¿½bernimmt der Autor keine Haftung.
 
 Alle Copyright - Hinweise innerhalb dieser Datei
-dürfen WEDER entfernt, NOCH verändert werden.
+dï¿½rfen WEDER entfernt, NOCH verï¿½ndert werden.
 ************************************************************
 Released under the GNU General Public License
 ************************************************************
@@ -173,7 +173,7 @@ if($action=="send") {
 	}
 	$spions=intval($_POST['spions']);
 
-	if($spions < 0 || $spions > $userdata['sol_spio']) {
+	if($spions <= 0 || $spions > $userdata['sol_spio']) {
 		show_error("LN_SPION_SPIO_ERROR",$modul_name);
 		exit();
 	}
